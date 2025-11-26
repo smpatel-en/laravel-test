096cfdead4786f1c3fea4110016b0154c5821314
thesujalpatel <patelsujal24102004@gmail.com>
2025-11-26 18:14:59 +0530

    Add Job and Employer models, factories, and migrations

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model
 {
+    use HasFactory;
+
     protected $table = "jobs_listing";
 
     protected $fillable = ["title", "location"];
--- database/migrations/2025_11_26_123100_create_employers_table.php
+++ database/migrations/2025_11_26_123100_create_employers_table.php
@@ -10,12 +10,9 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create("jobs_listing", function (Blueprint $table) {
+        Schema::create("employers", function (Blueprint $table) {
             $table->id();
-            $table->string("title");
-            $table->string("location");
-            $table->foreignId("employer_id")->constrained("employers");
-
+            $table->string("name");
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists("jobs_listing");
+        Schema::dropIfExists("employers");
     }
 };
